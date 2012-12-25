a27c0e294d083b5724fd789724b71580befedda3
nycz <pyntix@gmail.com>
2012-12-25 14:14:31 +0100

    Rewrite, now with scrolling and canvas size

--- kallistrate.py
+++ kallistrate.py
@@ -1,80 +1,102 @@
 #!/usr/bin/env python3
-import math, sys
-from PyQt4 import QtCore, QtGui
 
-class MainWindow(QtGui.QFrame):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        c = Canvas()
-        layout = QtGui.QVBoxLayout(self)
-        layout.addWidget(c)
-        self.setLayout(layout)
-        self.show()
+import math
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import Qt
+
 
 class Canvas(QtGui.QWidget):
-    def __init__(self):
-        super(Canvas, self).__init__()
-        self.init = True
-        self.setAttribute(QtCore.Qt.WA_OpaquePaintEvent)
-        self.color = QtCore.Qt.green
-        self.radius = 8
-        self.brush = QtGui.QBrush(self.color, QtCore.Qt.SolidPattern)
-        self.pen = QtGui.QPen(self.color)
-        self.lastPoint = None
-        self.drawQueue = []
+    def __init__(self, parent=None):
+        super(Canvas, self).__init__(parent)
+
+        self.setAttribute(Qt.WA_StaticContents)
+        self.modified = False
+        self.drawing = False
+        self.radius = 30
+        self.color = Qt.black
+        self.image = QtGui.QImage(800,800, QtGui.QImage.Format_RGB32)
+        self.image.fill(QtGui.qRgb(55, 155, 155))
+        self.brush = QtGui.QBrush(self.color, Qt.SolidPattern)
+        self.update()
+        self.lastPoint = QtCore.QPoint()
+        self.resize(self.image.size())
 
     def mousePressEvent(self, event):
-        self.lastPoint = None 
-        self.drawQueue.append(event.pos())
-        self.update()
+        if event.button() == QtCore.Qt.LeftButton:
+            self.lastPoint = event.pos()
+            self.drawLineTo(event.pos())
+            self.drawing = True
 
     def mouseMoveEvent(self, event):
-        self.drawQueue.append(event.pos())
-        self.update()
+        if (event.buttons() & QtCore.Qt.LeftButton) and self.drawing:
+            self.drawLineTo(event.pos())
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton and self.drawing:
+            self.drawing = False
 
     def paintEvent(self, event):
-        if self.init:
-            qp = QtGui.QPainter()
-            qp.begin(self)
-            qp.fillRect(self.contentsRect(), QtCore.Qt.white)
-            qp.end()
-            self.init = False
-            return
-
-        queue = self.drawQueue
-        self.drawQueue = []
-        qp = QtGui.QPainter()
-        qp.begin(self)
-        qp.setRenderHint(QtGui.QPainter.Antialiasing)
-        qp.setBrush(self.brush)
-        qp.setPen(self.pen)
-        for p in queue:
-            if self.lastPoint:
-                self.drawStroke(qp, self.lastPoint, p)
-            qp.drawEllipse(p, self.radius, self.radius)
-        if queue:
-            self.lastPoint = queue[-1]
-        qp.end()
-
-    def drawStroke(self, qp, p1, p2):
-        x1, y1 = p1.x(), p1.y()
-        x2, y2 = p2.x(), p2.y()
-
-        xn = x2-x1
-        yn = y2-y1
-        steps = math.hypot(xn, yn)/(self.radius/2)
-        if steps == 0:
-            return
-
-        xstep = xn/steps
-        ystep = yn/steps
-
-        for n in range(int(steps)+1):
-            qp.drawEllipse(QtCore.QPointF(x1+xstep*n, y1+ystep*n), self.radius, self.radius) 
+        painter = QtGui.QPainter(self)
+        painter.drawImage(0,0, self.image)
         
+    def drawLineTo(self, endPoint):
+        painter = QtGui.QPainter(self.image)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.setBrush(self.brush)
+        self.drawStroke(painter, self.lastPoint, endPoint)
+        self.modified = True
+
+        self.update()
+        self.lastPoint = QtCore.QPoint(endPoint)
+
+    def drawStroke(self, painter, firstPoint, endPoint):
+        if firstPoint != endPoint:
+            x1, y1 = firstPoint.x(), firstPoint.y()
+            x2, y2 = endPoint.x(), endPoint.y()
+
+            xn = x2-x1
+            yn = y2-y1
+            steps = math.hypot(xn, yn)/(self.radius/4)
+            if steps == 0:
+                return
+
+            xstep = xn/steps
+            ystep = yn/steps
+
+            for n in range(int(steps)+1):
+                painter.drawEllipse(QtCore.QPointF(x1+xstep*n, y1+ystep*n), 
+                                    self.radius, self.radius) 
+        painter.drawEllipse(endPoint, self.radius, self.radius)
+
+    def resizeImage(self, image, newSize):
+        if image.size() == newSize:
+            return
+
+        newImage = QtGui.QImage(newSize, QtGui.QImage.Format_RGB32)
+        newImage.fill(QtGui.qRgb(255, 255, 255))
+        painter = QtGui.QPainter(newImage)
+        painter.drawImage(QtCore.QPoint(0, 0), image)
+        self.image = newImage
+
+
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.drawingArea = Canvas()
+
+        scrollarea = QtGui.QScrollArea()
+        scrollarea.setWidget(self.drawingArea)
+
+        self.setCentralWidget(scrollarea)
+        self.setWindowTitle('Kallistrate')
 
 
 if __name__ == '__main__':
+
+    import sys
+
     app = QtGui.QApplication(sys.argv)
-    a = MainWindow()
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
